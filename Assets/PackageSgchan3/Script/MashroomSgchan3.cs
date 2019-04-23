3f202c30677734a4fd96b5f52c9a7c5899d508b0
LAPTOP-KAR0J5S8\suginomasashi <sgnmssuioaai@gmail.com>
2019-04-23 10:03:28 +0900

    Packageの複製を作成

---
+++ Assets/PackageSgchan3/Script/MashroomSgchan3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MashroomSgchan3 : MonoBehaviour
+{
+    public float jump=10000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            var normal= collision.GetContact(0).normal;
+            collision.transform.GetComponent<Rigidbody2D>().AddForce(-normal * jump);
+        }
+    }
+}
